3f79708f56f97a2f67586d0086c65412ee2bd5fc
Michael Cather <mgcather07@gmail.com>
2025-04-03 13:59:06 -0500

    Initial Commit

---
+++ rAITests/rAITests.swift
@@ -0,0 +1,16 @@
+//
+//  rAITests.swift
+//  rAITests
+//
+//  Created by Michael Cather on 4/3/25.
+//
+
+import Testing
+
+struct rAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
